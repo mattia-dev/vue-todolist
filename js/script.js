705c3da5c0cc5dc1c82da52a59b4059553134688
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-06-29 14:20:14 +0200

    created and linked files

---
+++ js/script.js
@@ -0,0 +1,22 @@
+new Vue(
+    {
+        el: '#app',
+        data: {
+            list: [
+                'walk the dog',
+                'go to the grocery store',
+                'change the light bulb',
+                'clean up the bathroom',
+                'call your friend'
+            ]
+        },
+        mounted() {
+            this.focusElement()
+        },
+        methods: {
+            focusElement() {
+                document.getElementById("input-field").focus();
+            },
+        }
+    }
+); 
